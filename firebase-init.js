3440ff6f731735bc254dd5b666e440a6f476885f
025ambrosio <ambrosiolucas025@gmail.com>
2024-12-11 17:34:52 -0300

    att login panel

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,7 @@
-// Importar funções do Firebase Modular SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
 
-// Configurações do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBAQ6TVoiHDNE58bz-hmYabP6FD6_iqEdQ",
   authDomain: "controle-de-gastos-3703a.firebaseapp.com",
@@ -13,12 +11,9 @@ const firebaseConfig = {
   appId: "1:697815545565:web:d38bc8df64b95b65234d34"
 };
 
-// Inicializar o Firebase App
 const app = initializeApp(firebaseConfig);
 
-// Inicializar módulos do Firebase
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exportar módulos para uso em outros arquivos
 export { auth, db };
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 import { auth, db } from './firebase-init.js';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 import { collection, query, where, getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
@@ -23,22 +21,20 @@ $(".veen .login-btn button").click(function () {
 $('#login').submit(function(e) {
   e.preventDefault();
 
-  const input = $('#login-email-username').val(); // Pode ser email ou username
+  const input = $('#login-email-username').val();
   const password = $('#login-password').val();
 
-  // Verificar se o input é um email ou nome de usuário
   let emailToLogin = input;
 
   if (!input.includes('@')) {
-    // Se não for um email (não contém '@'), é um username
     const q = query(collection(db, "users"), where("username", "==", input));
 
     getDocs(q)
       .then((querySnapshot) => {
         if (!querySnapshot.empty) {
           const userDoc = querySnapshot.docs[0].data();
-          emailToLogin = userDoc.email; // Pega o email associado ao username
-          loginWithEmail(emailToLogin, password); // Chama a função de login
+          emailToLogin = userDoc.email;
+          loginWithEmail(emailToLogin, password);
         } else {
           alert('Nome de usuário não encontrado');
         }
@@ -48,7 +44,6 @@ $('#login').submit(function(e) {
         alert('Erro ao buscar usuário');
       });
   } else {
-    // Caso já tenha um email, faz o login diretamente
     loginWithEmail(emailToLogin, password);
   }
 });
@@ -59,13 +54,13 @@ function loginWithEmail(email, password) {
     .then((userCredential) => {
       const user = userCredential.user;
       console.log('Usuário logado:', user);
-      window.location.href = 'home/home.html'; // Redireciona após login
+      window.location.href = 'home/home.html';
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.error('Erro de login:', errorCode, errorMessage);
-      alert('Erro de login: ' + errorMessage); // Exibe o erro
+      alert('Erro de login: ' + errorMessage); 
     });
 }
 
@@ -82,7 +77,6 @@ $('#register').submit(function(e) {
     .then((userCredential) => {
       const user = userCredential.user;
 
-      // Salvar dados adicionais no Firestore
       setDoc(doc(db, "users", user.uid), {
         name: name,
         username: username,
